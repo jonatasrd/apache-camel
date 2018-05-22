a3ad9b9f9c386a55bfb7a7800c20900f849f89a7
Jonatas Ramos <jonatasrd@gmail.com>
2018-05-22 17:00:18 -0300

    conversão dos arquivos, split das mensagens e nome dinamico

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,8 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -8,6 +10,27 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
-
-	}	
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				from("file:pedidos?delay=2s&noop=true").
+				split().
+					xpath("/pedido/itens/item").
+				filter().
+					xpath("/item/formato[text()='EBOOK']").
+				log("${id}").
+				marshal()
+					.xmljson().
+				log("${body}").
+				//setHeader("CamelFileName", simple("${file:name.noext}.json")).
+				//setHeader("CamelFileName", simple("${id}.json")).
+				setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
+				to("file:saida");
+			}
+		});
+		
+		context.start();
+		Thread.sleep(20000L);
+		context.stop();
+	}
 }
