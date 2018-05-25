ebfa6d28355d542ed37df85f21e02fb26d729502
Jonatas Ramos <jonatasrd@gmail.com>
2018-05-25 16:47:55 -0300

    adicionado chamada ao webService com metodo GET e passagem de parametros

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidosArquivo.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidosArquivo.java
@@ -3,10 +3,9 @@ package br.com.caelum.camel;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class RotaPedidos {
+public class RotaPedidosArquivo {
 
 	public static void main(String[] args) throws Exception {
 
@@ -15,20 +14,16 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=2s&noop=true").
-				setProperty("pedidoId", xpath("/pedido/id/text()")).
-			    setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
 				split().
 					xpath("/pedido/itens/item").
 				filter().
 					xpath("/item/formato[text()='EBOOK']").
-				setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+				log("${id}").
 				marshal()
 					.xmljson().
-				log("${id} - ${body}").
-				setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
-				setHeader(Exchange.HTTP_QUERY,   
-						simple("clienteId=${property.clienteId}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
-				to("http4://localhost:8080/webservices/ebook/item");
+				log("${body}").
+				setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json")).
+				to("file:saida");
 			}
 		});
 		
